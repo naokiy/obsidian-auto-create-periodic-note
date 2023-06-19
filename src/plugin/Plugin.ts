ffb0d1f8d646240fe30d0c0a1335963045c50583
naokiy <to_contact@naokiy.net>
2023-06-19 14:42:57 +0000

    :construction: Add scheduler

--- src/plugin/Plugin.ts
+++ src/plugin/Plugin.ts
@@ -3,17 +3,23 @@ import { DEFAULT_SETTINGS } from "../Settings";
 
 import { PluginSettingTab } from "./PluginSettingTab";
 
+import moment from "moment";
+import * as schedule from "node-schedule";
 import * as obsidian from "obsidian";
+import { createDailyNote } from "obsidian-daily-notes-interface";
 
 export class Plugin extends obsidian.Plugin {
   settings: Settings;
+  job?: schedule.Job;
 
   async onload() {
     await this.loadSettings();
     this.addSettingTab(new PluginSettingTab(this.app, this));
   }
 
-  onunload() {}
+  onunload() {
+    this.job?.cancel();
+  }
 
   async loadSettings() {
     this.settings = Object.assign(
@@ -25,5 +31,15 @@ export class Plugin extends obsidian.Plugin {
 
   async saveSettings() {
     await this.saveData(this.settings);
+  }
+
+  activateSchedule(): void {
+    const rule = new schedule.RecurrenceRule();
+    rule.hour = 0;
+    rule.minute = 0;
+    schedule.scheduleJob(rule, async () => {
+      const date = moment();
+      await createDailyNote(date);
+    });
   }
 }
